fb5446513b26f6585f0e241660b5486d024c68f4
Hephzibah Ofomi <h.ofomi@alustudent.com>
2026-05-15 00:54:44 +0000

    feat: update MyComponent styles and change header text

--- my-first-react-app/src/MyComponent.tsx
+++ my-first-react-app/src/MyComponent.tsx
@@ -4,7 +4,7 @@ import './MyComponent.css';
 const MyComponent: React.FC = () => {
   return (
     <div className="my-component">
-      <h1>Hello from MyComponent!</h1>
+      <h1>Welcome to React!</h1>
       <p>This is a paragraph of text within my component.</p>
     </div>
   );
